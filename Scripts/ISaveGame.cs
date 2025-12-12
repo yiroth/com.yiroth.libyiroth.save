9fc620d2796751543274bcfdd0e6495a5d8fa8f1
Alsiel Nokklair <alsielnokklair@gmail.com>
2025-12-12 13:47:02 +0300

    Save package

---
+++ Scripts/ISaveGame.cs
@@ -0,0 +1,8 @@
+﻿namespace LibYiroth.Save
+{
+    public interface ISaveGame
+    {
+        bool OnSaving(SaveManager saveManager);
+        bool OnLoading(SaveManager saveManager);
+    }
+}
---
+++ Scripts/SaveManager.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using LibYiroth;
+
+namespace LibYiroth.Save
+{
+    public class SaveManager : MonoBehaviour
+    {
+        private GameSlot _activeGameSlot;
+
+        private void Start()
+        {
+            _activeGameSlot = new GameSlot();
+        }
+
+        public bool SaveGameSlot(string slotName)
+        {
+            return false;
+        }
+
+        public GameSlot GetActiveGameSlot()
+        {
+            return _activeGameSlot;
+        }
+
+        public bool SaveVariable<T>(Data.Identification id, string variableName, T value)
+        {
+            if (_activeGameSlot == null)
+                return false;
+            
+            if(!id.IsValid())
+            {
+                Debug.LogError("SaveVariable: Level ID is not valid!");
+                return false;
+            }
+
+            if(string.IsNullOrEmpty(variableName))
+            {
+                Debug.LogError("SaveVariable: VariableName is null or empty!");
+                return false;
+            }
+            
+            // Create a new save variable container
+            SavedVariable sv = new SavedVariable(id, variableName, new Variant.Variant(value));
+            
+            // Create a key for the save variable container
+            SaveKey key = new SaveKey(ref sv.id, ref sv.variableName);
+            
+            // Add them to the slot
+            _activeGameSlot.SavedVariables.Add(key, sv);
+            _activeGameSlot.UniqueIdentifications.Add(sv.id);
+
+            return true;
+        }
+    }
+}
